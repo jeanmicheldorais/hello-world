d959dd025c2f8368cc60db7df4cbccf719a7ea06
jeanmicheldorais <101349015+jeanmicheldorais@users.noreply.github.com>
2022-05-06 10:27:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+var input = document.getElementById("focusedElement");
+var modal = document.getElementById("skipmodal");
+input.addEventListener("focus", function () {
+  modal.style.marginLeft = "0%";  
+});
+
+input.addEventListener("blur", function () {
+  modal.style.marginLeft = "100%";  
+});
+
 function FormSent() {
 
 var FormName;
@@ -12,12 +22,18 @@ FormDepartment = document.getElementById("depart").value;
 
 document.getElementById("responsebox").innerHTML = "<p>Your name is " + FormName + " " + FormLastName + ".</p>" + "<p>You work " + FormRadio + ".</p>" + "<p>Your department is " + FormDepartment + ".</p>";
 
+document.getElementById("responsebox").setAttribute("role", "alert");
+
 //document.getElementById("responsebox").innerHTML = FormRadio;
 
 
 //return true;
 
-}
+};
+
+
+
+
 
 // const myHeading = document.querySelector('h1');
 // myHeading.textContent = 'Hello world!';
